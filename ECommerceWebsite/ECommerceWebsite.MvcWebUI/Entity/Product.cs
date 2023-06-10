9ab196ce786c8a6e11b6b5b2a54d31393b9a2f68
Ramazan Eren Karakus <ramazanerenkarakus@gmail.com>
2023-06-10 22:18:37 +0300

    Product class is completed.

---
+++ ECommerceWebsite/ECommerceWebsite.MvcWebUI/Entity/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ECommerceWebsite.MvcWebUI.Entity
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public double ProductPrice { get; set; }
+        public int ProductStock { get; set; }
+        public bool IsApproved { get; set; }
+
+    }
+}
