a397334b02c9027597fc1de088038c07a32f2e23
Ramazan Eren Karakus <ramazanerenkarakus@gmail.com>
2023-06-10 22:41:39 +0300

    Context class is created.

---
+++ ECommerceWebsite/ECommerceWebsite.MvcWebUI/Entity/Context.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ECommerceWebsite.MvcWebUI.Entity
+{
+    public class Context : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories{ get; set; }
+
+    }
+}
