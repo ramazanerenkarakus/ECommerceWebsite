f6195389dcf11a44c02bed01b92d5ee4066e5214
Ramazan Eren Karakus <ramazanerenkarakus@gmail.com>
2023-06-10 22:57:09 +0300

    DataInitializer class is created.

---
+++ ECommerceWebsite/ECommerceWebsite.MvcWebUI/Entity/DataInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ECommerceWebsite.MvcWebUI.Entity
+{
+    public class DataInitializer : DropCreateDatabaseIfModelChanges<Context>
+    {
+        protected override void Seed(Context context)
+        {
+            base.Seed(context); 
+        }
+    }
+}
