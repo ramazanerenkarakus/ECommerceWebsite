bad4cbfe177d5c7302379c710a55700ceddcbf6a
Ramazan Eren Karakus <ramazanerenkarakus@gmail.com>
2023-06-10 23:19:33 +0300

    DataInitilaizer is completed.

--- ECommerceWebsite/ECommerceWebsite.MvcWebUI/Entity/DataInitializer.cs
+++ ECommerceWebsite/ECommerceWebsite.MvcWebUI/Entity/DataInitializer.cs
@@ -10,7 +10,45 @@ namespace ECommerceWebsite.MvcWebUI.Entity
     {
         protected override void Seed(Context context)
         {
-            base.Seed(context); 
+
+            var kategoriler = new List<Category>()
+            {
+
+                new Category(){CategoryName="Kamera",CategoryDescription="Kamera Ürünleri"},
+                new Category(){CategoryName="Bilgisayar",CategoryDescription="Bilgisayar Ürünleri"},
+                new Category(){CategoryName="Elektronik",CategoryDescription="Elektronik Ürünleri"},
+                new Category(){CategoryName="Telefon",CategoryDescription="Telefon Ürünleri"},
+                new Category(){CategoryName="Beyaz Eşya",CategoryDescription="Beyaz Eşya Ürünleri"}
+            };
+
+            foreach (var kategori in kategoriler)
+            {
+                context.Categories.Add(kategori);
+            }
+            context.SaveChanges();
+
+            var urunler = new List<Product>()
+            {
+                new Product(){ProductName="Samsung Kamera",ProductDescription="aaaaaaaa",ProductPrice=4500, ProductStock=100,IsApproved=true,CategoryId=1},
+                new Product(){ProductName="Lenovo Kamera",ProductDescription="aaaaaaaa",ProductPrice=4500, ProductStock=100,IsApproved=true,CategoryId=1},
+                new Product(){ProductName="Samsung Bilgisayar",ProductDescription="bbbbbbbb",ProductPrice=2000, ProductStock=200,IsApproved=false,CategoryId=2},
+                new Product(){ProductName="Lenovo Bilgisayar",ProductDescription="bbbbbbbb",ProductPrice=5000, ProductStock=200,IsApproved=true,CategoryId=2},
+                new Product(){ProductName="Samsung Elektronik",ProductDescription="asdasdas",ProductPrice=3000, ProductStock=300,IsApproved=true,CategoryId=3},
+                new Product(){ProductName="Lenovo Elektronik",ProductDescription="asdasdas",ProductPrice=7000, ProductStock=300,IsApproved=false,CategoryId=3},
+                new Product(){ProductName="Samsung Telefon",ProductDescription="ccccccccc",ProductPrice=4000, ProductStock=400,IsApproved=true,CategoryId=4},
+                new Product(){ProductName="Lenovo Telefon",ProductDescription="ccccccccc",ProductPrice=8000, ProductStock=400,IsApproved=true,CategoryId=4},
+                new Product(){ProductName="Samsung Beyaz Eşya",ProductDescription="dddddddd",ProductPrice=5000, ProductStock=500,IsApproved=false,CategoryId=5},
+                new Product(){ProductName="Lenovo Beyaz Eşya",ProductDescription="dddddddd",ProductPrice=6000, ProductStock=500,IsApproved=true,CategoryId=5}
+            };
+
+            foreach(var urun in urunler)
+            {
+                context.Products.Add(urun);
+            }
+            context.SaveChanges();
+
+
+            base.Seed(context);
         }
     }
 }
