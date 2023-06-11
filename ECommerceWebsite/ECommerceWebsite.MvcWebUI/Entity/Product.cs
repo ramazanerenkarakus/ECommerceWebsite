9105dd7ed4748d76c47f1f4299eecca3f4b2d332
Ramazan Eren Karakus <ramazanerenkarakus@gmail.com>
2023-06-11 21:44:13 +0300

    ProductIndex html side editing.

--- ECommerceWebsite/ECommerceWebsite.MvcWebUI/Entity/Product.cs
+++ ECommerceWebsite/ECommerceWebsite.MvcWebUI/Entity/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -8,12 +9,16 @@ namespace ECommerceWebsite.MvcWebUI.Entity
     public class Product
     {
         public int Id { get; set; }
+        [DisplayName("Ürün Adı")]
         public string ProductName { get; set; }
+        [DisplayName("Ürün Açıklaması")]
         public string ProductDescription { get; set; }
         public double ProductPrice { get; set; }
         public int ProductStock { get; set; }
         public string Image{ get; set; }
+        [DisplayName("Ana Sayfada mı?")]
         public bool IsHome { get; set; }
+        [DisplayName("Onaylı mı ?")]
         public bool IsApproved { get; set; }
 
         public int CategoryId { get; set; }
