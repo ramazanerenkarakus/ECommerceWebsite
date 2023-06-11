2bd1fa2ab009f39f47a80732823c5174503458d3
Ramazan Eren Karakus <ramazanerenkarakus@gmail.com>
2023-06-12 01:39:30 +0300

    Login Class is created.

---
+++ ECommerceWebsite/ECommerceWebsite.MvcWebUI/Models/Login.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace ECommerceWebsite.MvcWebUI.Models
+{
+    public class Login
+    {
+        [Required]
+        [DisplayName("Kullanıcı Adınız")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DisplayName("Şifre")]
+        public string Password { get; set; }
+
+
+        [DisplayName("Beni Hatırla")]
+        public bool RememberMe { get; set; }
+    }
+}
