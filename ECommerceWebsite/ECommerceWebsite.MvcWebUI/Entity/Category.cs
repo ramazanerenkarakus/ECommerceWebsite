224955b52eae8ced2a69c751213aa13cea76aa43
Ramazan Eren Karakus <ramazanerenkarakus@gmail.com>
2023-06-11 17:37:28 +0300

    CategoryCreate editing.

--- ECommerceWebsite/ECommerceWebsite.MvcWebUI/Entity/Category.cs
+++ ECommerceWebsite/ECommerceWebsite.MvcWebUI/Entity/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,9 @@ namespace ECommerceWebsite.MvcWebUI.Entity
     public class Category
     {
         public int Id { get; set; }
+        [DisplayName("Kategori Adı")]
         public string CategoryName { get; set; }
+        [DisplayName("Açıklama")]
         public string CategoryDescription { get; set; }
 
         public List<Product> Products { get; set; }
