1673aaff7c2bae239ba1c3efcb6e6dedbdfc49c4
Ramazan Eren Karakus <ramazanerenkarakus@gmail.com>
2023-06-12 01:07:55 +0300

    Identity packages are installed.

--- ECommerceWebsite/ECommerceWebsite.MvcWebUI/Entity/Category.cs
+++ ECommerceWebsite/ECommerceWebsite.MvcWebUI/Entity/Category.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -10,6 +11,7 @@ namespace ECommerceWebsite.MvcWebUI.Entity
     {
         public int Id { get; set; }
         [DisplayName("Kategori Adı")]
+        [StringLength(maximumLength:25,ErrorMessage ="En fazla 25 karakter girebilirsiniz.")]
         public string CategoryName { get; set; }
         [DisplayName("Açıklama")]
         public string CategoryDescription { get; set; }
