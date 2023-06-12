aacee60bb973208430c6b25197e5bdda02a58452
Ramazan Eren Karakus <ramazanerenkarakus@gmail.com>
2023-06-12 04:08:18 +0300

    Role creating is completed.

--- ECommerceWebsite/ECommerceWebsite.MvcWebUI/Identity/IdentityInitializer.cs
+++ ECommerceWebsite/ECommerceWebsite.MvcWebUI/Identity/IdentityInitializer.cs
@@ -1,4 +1,6 @@
 ﻿using ECommerceWebsite.MvcWebUI.Entity;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -7,7 +9,20 @@ using System.Web;
 
 namespace ECommerceWebsite.MvcWebUI.Identity
 {
-    public class IdentityInitializer : DropCreateDatabaseIfModelChanges<Context>
+    public class IdentityInitializer : CreateDatabaseIfNotExists<IdentityDataContext>
     {
+        protected override void Seed(IdentityDataContext context)
+        {
+            //Roller
+            if (!context.Roles.Any(i=>i.Name=="admin"))
+            {
+                var store = new RoleStore<IdentityRole>(context);
+                var manager = new RoleManager<IdentityRole>(store);
+                var role = new ApplicationRole("admin", "yönetici rolü");
+                manager.Create(role);
+            }
+
+            base.Seed(context);
+        }
     }
 }
