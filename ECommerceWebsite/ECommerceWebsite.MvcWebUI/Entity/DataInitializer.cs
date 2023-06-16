b5dab7c878620cf36ea813362588d48f28bb272b
Ramazan Eren Karakus <ramazanerenkarakus@gmail.com>
2023-06-16 23:53:58 +0300

    Last editions in market side.

--- ECommerceWebsite/ECommerceWebsite.MvcWebUI/Entity/DataInitializer.cs
+++ ECommerceWebsite/ECommerceWebsite.MvcWebUI/Entity/DataInitializer.cs
@@ -29,14 +29,14 @@ namespace ECommerceWebsite.MvcWebUI.Entity
 
             var products = new List<Product>()
             {
-                new Product(){ProductName="Samsung Kamera",ProductDescription="aaaaaaaa",ProductPrice=4500, ProductStock=100,IsApproved=true,CategoryId=1,IsHome=true,Image="cameraphoto.png"},
-                new Product(){ProductName="Lenovo Kamera",ProductDescription="aaaaaaaa",ProductPrice=4500, ProductStock=100,IsApproved=true,CategoryId=1},
-                new Product(){ProductName="Samsung Bilgisayar",ProductDescription="bbbbbbbb",ProductPrice=2000, ProductStock=200,IsApproved=false,CategoryId=2,Image="cameraphoto.png"},
-                new Product(){ProductName="Lenovo Bilgisayar",ProductDescription="bbbbbbbb",ProductPrice=5000, ProductStock=200,IsApproved=true,CategoryId=2,IsHome=true,Image="cameraphoto.png"},
-                new Product(){ProductName="Samsung Elektronik",ProductDescription="asdasdas",ProductPrice=3000, ProductStock=300,IsApproved=true,CategoryId=3,Image="cameraphoto.png"},
-                new Product(){ProductName="Lenovo Elektronik",ProductDescription="asdasdas",ProductPrice=7000, ProductStock=300,IsApproved=false,CategoryId=3,IsHome=true,Image="cameraphoto.png"},
-                new Product(){ProductName="Samsung Telefon",ProductDescription="ccccccccc",ProductPrice=4000, ProductStock=400,IsApproved=true,CategoryId=4,IsHome=true,Image="cameraphoto.png"},
-                new Product(){ProductName="Lenovo Telefon",ProductDescription="ccccccccc",ProductPrice=8000, ProductStock=400,IsApproved=true,CategoryId=4,Image="cameraphoto.png"},
+                new Product(){ProductName="Samsung Kamera",ProductDescription="Samsung Kamera",ProductPrice=4500, ProductStock=100,IsApproved=true,CategoryId=1,IsHome=true,Image="noImage.jpg"},
+                new Product(){ProductName="Lenovo Kamera",ProductDescription="Lenovo Kamera Açıklaması",ProductPrice=4500, ProductStock=100,IsApproved=true,CategoryId=1},
+                new Product(){ProductName="Samsung Bilgisayar",ProductDescription="Samsung Bilgisayar",ProductPrice=2000, ProductStock=200,IsApproved=false,CategoryId=2,Image="noImage.jpg"},
+                new Product(){ProductName="Lenovo Bilgisayar",ProductDescription="Lenovo Bilgisayar",ProductPrice=5000, ProductStock=200,IsApproved=true,CategoryId=2,IsHome=true,Image="cameraphoto.png"},
+                new Product(){ProductName="Samsung Elektronik",ProductDescription="Samsung Elektronik",ProductPrice=3000, ProductStock=300,IsApproved=true,CategoryId=3,Image="noImage.jpg"},
+                new Product(){ProductName="Lenovo Elektronik",ProductDescription="Lenovo Elektronik",ProductPrice=7000, ProductStock=300,IsApproved=false,CategoryId=3,IsHome=true,Image="noImage.jpg"},
+                new Product(){ProductName="Samsung Telefon",ProductDescription="Açıklama",ProductPrice=4000, ProductStock=400,IsApproved=true,CategoryId=4,IsHome=true,Image="noImage.jpg"},
+                new Product(){ProductName="Lenovo Telefon",ProductDescription="Açıklama",ProductPrice=8000, ProductStock=400,IsApproved=true,CategoryId=4,Image="noImage.jpg"},
             };
 
             foreach(var product in products)
